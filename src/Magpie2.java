fdb95198da05cd1abdb94c80f8944c5342c6b4aa
shan <Striddle@hotmail.com>
2014-10-08 11:20:37 -0400

    stuff

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -48,6 +48,35 @@ public class Magpie2
 		}
 		return response;
 	}
+    private int findKeyword(String statement, String goal, int startPos)
+
+    {
+        String phrase = statement.trim();
+        int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
+
+        while (psn >= 0)
+        {
+            String before = " ", after = " ";
+            if (psn > 0)
+            {
+                before = phrase.substring(psn - 1, psn).toLowerCase();
+            }
+            if (psn + goal.length() < phrase.length()) {
+            {
+                after = phrase.substring(psn + goal.length(), psn + goal.length() + 1).toLowerCase();
+
+            }
+
+            if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) && ((after.compareTo("a") < 0) || (after.compareTo("z") > 0))) {
+            }
+                    return psn;
+
+            }
+                psn = phrase.indexOf(goal.toLowerCase(), psn + 1);
+           }
+            return -1;
+        }
+
 
 	/**
 	 * Pick a default response to use if nothing else fits.
